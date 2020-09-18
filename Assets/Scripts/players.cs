6d3b91e2d15822043b8879ead0c61203f6155286
Paola Garbato <paolagarbato@gmail.com>
2020-09-18 14:53:54 -0300

    Movimentação dos players - implementando no player 1

---
+++ Assets/Scripts/players.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class players : MonoBehaviour
+{
+    public double velocidade; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, Input.GetAxis("Vertical") * Time.deltaTime, 0);
+        
+    }
+}
